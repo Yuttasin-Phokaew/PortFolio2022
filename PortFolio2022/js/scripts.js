8289baade99e746e7b3dfee6fbee1745835d0648
Yuttasin-Phokaew <71009316+Yuttasin-Phokaew@users.noreply.github.com>
2022-10-06 19:18:41 +0700

    Add files via upload

---
+++ PortFolio2022/js/scripts.js
@@ -0,0 +1,45 @@
+    (function ($) {
+    "use strict";
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 72,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+    // Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    navbarCollapse();
+    // Collapse the navbar
+    $(window).scroll(navbarCollapse);
+})(jQuery);
